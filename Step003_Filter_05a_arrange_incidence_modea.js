f4e2fdc49425ab01ae737866afdfe66b143f33e6
Grazieli Rodigheri <grazielirodigheri@gmail.com>
2024-08-02 17:52:21 -0300

    Update to Collection 9

--- Step003_Filter_05a_arrange_incidence_modea.js
+++ Step003_Filter_05a_arrange_incidence_modea.js
@@ -1,40 +1,43 @@
 // MAPBIOMAS PAMPA
-// COLLECTION 08
-// AUTHOR: Juliano Schirmbeck
-// DATE: August 2020
-//
-//  Randon Forest to region 06
+// COLLECTION 09
+// AUTHOR: Juliano Schirmbeck 
+// UPDATE: May 2020
 
-var version = '07'
-var col = '8'
-
+// ***************************************************************************************
+// Define as variáveis referentes a versão da coleção ou dos filtros
+var version = '12'
+var col = '9'
 var versionOut = version + '_pre_incidentes'
 var versionIn = version + '_gap'
+
+// Define as regiões: [1,2,3,4,5,6,7]
+var regioes = [1,2,3,4,5,6,7]
+
+// ***************************************************************************************
+
 
 var anos = [                            '1985','1986','1987','1988','1989','1990',
             '1991','1992','1993','1994','1995','1996','1997','1998','1999','2000',
             '2001','2002','2003','2004','2005','2006','2007','2008','2009','2010',
             '2011','2012','2013','2014','2015','2016','2017','2018','2019','2020',
-            '2021','2022'];
+            '2021','2022','2023'];
 
 var dir_filtros = 'projects/mapbiomas-workspace/AMOSTRAS/col' + col + '/PAMPA/class_col_' + col + '_filtros/'
-var regioesCollection = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/REGIOES/VETOR/PAMPA_regioes_col05_buff')
+var regioesCollection = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/REGIOES/VETOR/PAMPA_regioes_col09_buff')
 
 var palettes = require('users/mapbiomas/modules:Palettes.js');
 
 var vis = {
     'min': 0,
-    'max': 34,
-    'palette': palettes.get('classification2')
+    'max': 62,
+    'palette': palettes.get('classification8')
 };
 var visParMedian = {'bands':['median_swir1','median_nir','median_red'], 'gain':[0.08, 0.06,0.2],'gamma':0.5 };
 
 //editar os parametros
 var classeIds =    [3,11,12,21,22,29,33]
 var newClasseIds = [3,11,12,21,22,29,33]
- 
-var regioes = [1,2,3,4,5,6,
-                7]
+
 
 for (var i_regiao=0;i_regiao<regioes.length; i_regiao++){
     var regiao = regioes[i_regiao];
@@ -72,8 +75,8 @@ for (var i_regiao=0;i_regiao<regioes.length; i_regiao++){
   var vis2 = {
       'bands': '1987',
       'min': 0,
-      'max': 34,
-      'palette': palettes.get('classification2')
+      'max': 62,
+      'palette': palettes.get('classification8')
   };
   
   var palette_incidence = ["#C8C8C8","#FED266","#FBA713","#cb701b", "#cb701b", "#a95512", "#a95512", "#662000",  "#662000", "#cb181d"]
@@ -84,10 +87,10 @@ for (var i_regiao=0;i_regiao<regioes.length; i_regiao++){
                                   'classification_2005', 'classification_2006', 'classification_2007', 'classification_2008', 'classification_2009', 
                                   'classification_2010', 'classification_2011', 'classification_2012', 'classification_2013', 'classification_2014',
                                   'classification_2015', 'classification_2016', 'classification_2017', 'classification_2018', 'classification_2019',
-                                  'classification_2020','classification_2021','classification_2022'],
+                                  'classification_2020','classification_2021','classification_2022','classification_2023'],
                                  ['1985', '1986', '1987','1988', '1989', '1990','1991', '1992', '1993','1994', '1995', '1996','1997', '1998', '1999',
                                   '2000', '2001', '2002','2003', '2004', '2005','2006', '2007', '2008','2009', '2010', '2011','2012', '2013', '2014',
-                                  '2015', '2016', '2017', '2018','2019','2020','2021','2022'])
+                                  '2015', '2016', '2017', '2018','2019','2020','2021','2022','2023'])
   Map.addLayer(image_in, vis2, 'MapBiomas');
   
   Map.addLayer(image_incidence, {}, "incidents original");
